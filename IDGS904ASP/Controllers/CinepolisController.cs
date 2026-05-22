c6f5042c0c39bc8e583b8f182369e1cc78eec5a4
IDGS-904-23001531 <stefaniapro5@gmail.com>
2026-05-21 19:25:02 -0600

    Cinepolis 21/05/2026

---
+++ IDGS904ASP/Controllers/CinepolisController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IDGS904ASP.Models;
+
+namespace IDGS904ASP.Controllers
+{
+    public class CinepolisController : Controller
+    {
+        // GET: Cinepolis
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Cinepolis cine)
+        {
+            double precioBoleto = 12;
+            double subtotal = cine.CantidadBoletos * precioBoleto;
+            double total = subtotal;
+
+            if(cine.CantidadBoletos > 5)
+            {
+                total = subtotal - (subtotal * 0.15);
+            }
+            else if (cine.CantidadBoletos >= 3 && cine.CantidadBoletos <= 5)
+            {
+                total = subtotal - (subtotal * 0.10);
+            }
+
+            if (cine.Tarjeta)
+            {
+                total = total - (total * 0.10);
+            }
+
+            cine.TotalPagar = total;
+            return View(cine);
+        }
+    }
+}
